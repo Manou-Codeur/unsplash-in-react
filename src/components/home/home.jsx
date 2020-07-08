daa4637ff4914735fbeddf623c26a05e316142c1
khaled-codeur <manou-codeur@outlook.com>
2020-07-08 23:33:02 +0100

    header-home style done

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -11,7 +11,7 @@ class Home extends Component {
     return (
       <div className="home">
         <Headerhome />
-        <Picture />
+        {/* <Picture /> */}
       </div>
     );
   }
--- src/sub-components/header-home/headerHome.jsx
+++ src/sub-components/header-home/headerHome.jsx
@@ -1,28 +1,39 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
+import { Link } from "react-router-dom";
 
+import menuBar from "../../assets/img/menu.svg";
+import searchIcon from "../../assets/img/search.svg";
+import cameraIcon from "../../assets/img/camera.svg";
 import "./headerHome.scss";
 
 const Headerhome = () => {
   return (
     <div className="header-home">
       <nav>
-        <div className="inner-nav">
-          <p className="author">Made by A.Salim</p>
-          <div className="links">
-            <NavLink className="link" activeClassName="activeLink" to="./login">
-              Login
-            </NavLink>
-            <NavLink
-              className="link"
-              activeClassName="activeLink"
-              to="./singup"
-            >
-              Singup
-            </NavLink>
-          </div>
+        <div className="fake-div"></div>
+        <p className="author">Made by A.Salim</p>
+        <div className="links">
+          <Link className="link" to="./login">
+            Login
+          </Link>
+          <Link className="link" to="./singup">
+            Singup
+          </Link>
         </div>
       </nav>
+
+      <div className="header-content">
+        <img className="menu" src={menuBar} alt="menu icon" />
+        <div className="middle-content">
+          <img src={cameraIcon} alt="camera icon" />
+          <p className="logo-name">UNSPLASH</p>
+          <p className="for-margin">
+            Free high-resolution photos every 10 days
+          </p>
+          <button>Subscribe</button>
+        </div>
+        <img className="search" src={searchIcon} alt="search icon" />
+      </div>
     </div>
   );
 };
