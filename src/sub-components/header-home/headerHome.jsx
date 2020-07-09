8c26e91faa19c928c3dc067dc11c74e9d8fb739c
khaled-codeur <manou-codeur@outlook.com>
2020-07-09 13:10:46 +0100

    start css animation on header-home

--- src/sub-components/header-home/headerHome.jsx
+++ src/sub-components/header-home/headerHome.jsx
@@ -11,7 +11,7 @@ const Headerhome = () => {
     <div className="header-home">
       <nav>
         <div className="fake-div"></div>
-        <p className="author">Made by A.Salim</p>
+        <p className="author">Made by Salim</p>
         <div className="links">
           <Link className="link" to="./login">
             Login
@@ -26,7 +26,7 @@ const Headerhome = () => {
         <img className="menu" src={menuBar} alt="menu icon" />
         <div className="middle-content">
           <img src={cameraIcon} alt="camera icon" />
-          <p className="logo-name">UNSPLASH</p>
+          <p className="logo-name">MYSPLASH</p>
           <p className="for-margin">
             Free high-resolution photos every 10 days
           </p>
