d198271fb1d73fa40075c04d144e9745633a87cf
khaled-codeur <manou-codeur@outlook.com>
2020-07-09 19:14:43 +0100

    added the blur filter

--- src/sub-components/menu/menu.jsx
+++ src/sub-components/menu/menu.jsx
@@ -13,48 +13,53 @@ const Menu = ({ menuAsked }) => {
   const menuContext = useContext(Context);
 
   return (
-    <div className="menu" style={!menuAsked ? { display: "none" } : null}>
-      <div className="close-containner">
-        <img
-          className="close-icon"
-          src={closeIcon}
-          alt="close icon"
-          onClick={menuContext.closeMenu}
-        />
-      </div>
-
-      <div className="user-profile">
-        <div className="pp"></div>
-        <div className="user-info">
-          <h4>Ayache Salim</h4>
-          <p>logout</p>
+    <div
+      className="menu-containner"
+      style={!menuAsked ? { display: "none" } : null}
+    >
+      <div className="menu">
+        <div className="close-containner">
+          <img
+            className="close-icon"
+            src={closeIcon}
+            alt="close icon"
+            onClick={menuContext.closeMenu}
+          />
         </div>
-      </div>
 
-      <div className="browse-section">
-        <p className="head">Browse</p>
+        <div className="user-profile">
+          <div className="pp"></div>
+          <div className="user-info">
+            <h4>Ayache Salim</h4>
+            <p>logout</p>
+          </div>
+        </div>
 
-        <p>Featured</p>
-        <p>Popular</p>
-        <p>New</p>
-        <p>Collections</p>
-      </div>
+        <div className="browse-section">
+          <p className="head">Browse</p>
 
-      <div className="explore-section">
-        <p className="head">Explore</p>
+          <p>Featured</p>
+          <p>Popular</p>
+          <p>New</p>
+          <p>Collections</p>
+        </div>
 
-        <p>Made With Unsplash</p>
-        <p>Tag Photos</p>
-        <p>Developers</p>
-        <p>Backstage</p>
-        <p>Submit a photo</p>
-      </div>
+        <div className="explore-section">
+          <p className="head">Explore</p>
 
-      <div className="social-media">
-        <img className="social-icon" src={instaIcon} alt="Instagam" />
-        <img className="social-icon" src={facebookIcon} alt="Facebook" />
-        <img className="social-icon" src={pinIcon} alt="pinterest" />
-        <img className="social-icon" src={twitterIcon} alt="Twitter" />
+          <p>Made With Unsplash</p>
+          <p>Tag Photos</p>
+          <p>Developers</p>
+          <p>Backstage</p>
+          <p>Submit a photo</p>
+        </div>
+
+        <div className="social-media">
+          <img className="social-icon" src={instaIcon} alt="Instagam" />
+          <img className="social-icon" src={facebookIcon} alt="Facebook" />
+          <img className="social-icon" src={pinIcon} alt="pinterest" />
+          <img className="social-icon" src={twitterIcon} alt="Twitter" />
+        </div>
       </div>
     </div>
   );
