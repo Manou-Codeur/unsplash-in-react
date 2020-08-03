0c12fec65e6aafe035311e363a26ed9513a75d29
khaled-codeur <manou-codeur@outlook.com>
2020-08-03 14:45:09 +0100

    start working around tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,16 @@
-// import React from "react";
-// import { render } from "@testing-library/react";
-// import App from "./App";
+import React from "react";
+import { render } from "@testing-library/react";
+import App from "./App";
 
-// test("renders learn react link", () => {
-//   const { getByText } = render(<App />);
-//   const linkElement = getByText(/learn react/i);
-//   expect(linkElement).toBeInTheDocument();
-// });
+const sum = (a, b) => a + b;
+
+describe("first test", () => {
+  test("test one", () => {
+    expect(true).toBe(true);
+  });
+
+  test("test two", () => {
+    const result = sum(4, 2);
+    expect(result).toBe(6);
+  });
+});
