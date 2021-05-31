76596a0199230696b684339a430e0b568fab0b35
khaled-codeur <manou-codeur@outlook.com>
2021-05-31 02:14:01 +0100

    swithch the <App /> from class comp to funct comp

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component, Fragment } from "react";
+import React, { useEffect, Fragment, useState, useContext } from "react";
 import { Route, Switch, Redirect } from "react-router-dom";
 
 import FirebaseContext from "./services/firebase/firebaseContext";
@@ -10,80 +10,74 @@ import Login from "./components/login/login";
 import Singup from "./components/singup/singup";
 import Notfound from "./components/not-found/notFound";
 
-class App extends Component {
-  state = {
-    userAuth: null,
-  };
+const App = props => {
+  const [userAuth, setUserAuth] = useState(null);
 
-  static contextType = FirebaseContext;
+  const firebase = useContext(FirebaseContext);
 
-  componentDidMount() {
-    this.context.isUserAuthenticated(userInfo => {
-      this.setState({ userAuth: userInfo });
+  useEffect(() => {
+    firebase.isUserAuthenticated(userInfo => {
+      setUserAuth(userInfo);
     });
-  }
+  }, []);
 
-  render() {
-    const { userAuth } = this.state;
+  return (
+    <Fragment>
+      <Switch>
+        <Route
+          path="/profile/:username"
+          render={props =>
+            userAuth ? (
+              <Userprofile {...props} userAuth={userAuth} />
+            ) : (
+              <Redirect
+                to={{
+                  pathname: "/login",
+                  state: { from: props.location.pathname },
+                }}
+              />
+            )
+          }
+        />
 
-    return (
-      <Fragment>
-        <Switch>
-          <Route
-            path="/profile/:username"
-            render={props =>
-              userAuth ? (
-                <Userprofile {...props} userAuth={userAuth} />
-              ) : (
-                <Redirect
-                  to={{
-                    pathname: "/login",
-                    state: { from: props.location.pathname },
-                  }}
-                />
-              )
-            }
-          />
+        <Route
+          path="/picture/:id"
+          render={props =>
+            userAuth ? (
+              <Fullimage {...props} userAuth={userAuth} />
+            ) : (
+              <Redirect
+                to={{
+                  pathname: "/login",
+                  state: { from: props.location.pathname },
+                }}
+              />
+            )
+          }
+        />
 
-          <Route
-            path="/picture/:id"
-            render={props =>
-              userAuth ? (
-                <Fullimage {...props} userAuth={userAuth} />
-              ) : (
-                <Redirect
-                  to={{
-                    pathname: "/login",
-                    state: { from: props.location.pathname },
-                  }}
-                />
-              )
-            }
-          />
+        <Route
+          path="/search"
+          render={props => <Home {...props} search={true} />}
+        />
 
-          <Route
-            path="/search"
-            render={props => <Home {...props} search={true} />}
-          />
+        <Route
+          path="/login"
+          render={props => <Login {...props} userAuth={userAuth} />}
+        />
 
-          <Route
-            path="/login"
-            render={props => <Login {...props} userAuth={userAuth} />}
-          />
+        <Route path="/singup" render={props => <Singup {...props} />} />
 
-          <Route path="/singup" render={props => <Singup {...props} />} />
+        <Route path="/home" exact component={Home} />
 
-          <Route path="/home" exact component={Home} />
+        <Route path="/notFound" component={Notfound} />
 
-          <Route path="/notFound" component={Notfound} />
+        <Redirect from="/" exact to="/home" />
 
-          <Redirect from="/" exact to="/home" />
-
-          <Redirect to="/notFound" />
-        </Switch>
-      </Fragment>
-    );
-  }
-}
+        <Redirect to="/notFound" />
+      </Switch>
+    </Fragment>
+  );
+};
 
 export default App;
